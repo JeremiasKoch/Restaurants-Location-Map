2c64b4de48e7b04f2e32d93b22731f99f9d5c6c6
JeremiasKoch <jeremiaskoch.92@gmail.com>
2021-10-16 11:04:08 -0300

    fix: ts

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,43 @@
 import React, { useState, useEffect } from "react";
-import MapContainer from "../../components/map";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
-import axios from "axios";
+import MapContainer from "../../components/map";
+import { ILocationData } from "../../types/location";
+
+const defaultLocation = {
+  address: "",
+  applicant: "",
+  approved: "",
+  block: "",
+  blocklot: "",
+  cnn: "",
+  expirationdate: "",
+  facilitytype: "",
+  fooditems: "",
+  latitude: "",
+  location: { human_address: "", latitude: "", longitude: "" },
+  locationdescription: "",
+  longitude: "",
+  lot: "",
+  objectid: "",
+  permit: "",
+  priorpermit: "",
+  received: "",
+  schedule: "",
+  status: "",
+  x: "",
+  y: "",
+};
+
 
 const Dashboard = () => {
-  const [fetchFoodLocation, setFetchFoodLocation] = useState<any[]>([]);
+  const [fetchFoodLocation, setFetchFoodLocation] = useState<ILocationData[]>([]);
 
   const fetchLocations = async () => {
     axios
       .get("https://data.sfgov.org/resource/rqzj-sfat.json")
-      .then((resp: any) => setFetchFoodLocation(resp.data))
-      .catch((err: any) => setFetchFoodLocation(err));
-  };
+      .then((resp: AxiosResponse<never>) => setFetchFoodLocation(resp.data))
+      .catch((err: Error | AxiosError) => setFetchFoodLocation([ defaultLocation]))};
 
   useEffect(() => {
     fetchLocations();
